23a311605261d87d88ce09db3322e2d28ad32e7b
Idrisijas <official.idrisiajs@gmail.com>
2026-07-12 12:39:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
-window.IDRISIAJS_CONFIG={YOUTUBE_API_KEY:"AIzaSyCUW9NnKjbentSzowFnIa_ZRzdQ86ovZFs",CHANNEL_HANDLE:"idrisiajs",SHORTS_PLAYLIST_ID:"PLarXnt0POJtc",PLAYLISTS:{fisch:"PLbcfD4ofr4hg",garden:"https://youtube.com/playlist?list=PLEfHtPtpDOBo&si=TpxN8_JjK-6v_1R_",doors:"PASTE_DOORS_PLAYLIST_ID_HERE"},LATEST_VIDEO_COUNT:6,SHORT_COUNT:4,GAME_VIDEO_COUNT:3};
+ window.IDRISIAJS_CONFIG = {
+  YOUTUBE_API_KEY: "AIzaSyCUW9NnKjbentSzowFnIa_ZRzdQ86ovZFs",
+  CHANNEL_HANDLE: "idrisiajs",
+
+  // Shorts playlist ID only
+  SHORTS_PLAYLIST_ID: "PLarXnt0POJtc",
+
+  PLAYLISTS: {
+    // Fisch playlist ID only
+    fisch: "PLbcfD4ofr4hg",
+
+    // Garden playlist ID only
+    garden: "PLEfHtPtpDOBo",
+
+    // Replace when you have a DOORS playlist
+    doors: ""
+  },
+
+  LATEST_VIDEO_COUNT: 6,
+  SHORT_COUNT: 4,
+  GAME_VIDEO_COUNT: 3
+};
